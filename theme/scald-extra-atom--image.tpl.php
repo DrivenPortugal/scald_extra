797e2caa1a788362aa4c0eb3fc554b7c0fd141e1
Ana Silva <ana.silva@driven.pt>
2017-08-17 10:00:21 +0100

    Scald atom image update + styles for modal add image form and atom images (fullwidth, teaser and image description).

--- theme/scald-extra-atom--image.tpl.php
+++ theme/scald-extra-atom--image.tpl.php
@@ -1,21 +1,25 @@
-<div class="atom atom-image <?php print $align ?> <?php print $decoded_options['additionalClasses']?>">
+<div
+    class="atom atom-image atom--context-<?php print $context; ?> atom--align-<?php print $align ?>"
+    style="width: <?php print $atom->base_entity->width; ?>px"
+>
   <div class="atom-<?php print $atom->sid ?>">
     <div class="content">
       <?php if (!empty($url)): ?>
-        <a href="<?php print $url ?>">
-      <?php endif; ?>
-      <img
-        title="<?php print empty($atom->title) ? '' : $atom->title ?>"
-        src="<?php print $src ?>"
-      />
-      <?php if (!empty($url)): ?>
-        </a>
-      <?php endif; ?>
-      <?php if (!empty($description)): ?>
-        <div class="description">
-          <?php print $description ?>
+      <a href="<?php print $url ?>">
+        <?php endif; ?>
+
+        <img class="img-fluid" alt="<?php print empty($atom->title) ? '' : $atom->title ?>" src="<?php print $src ?>"/>
+
+        <?php if (!empty($url)): ?>
+      </a>
+    <?php endif; ?>
+
+      <?php if (!empty($atom->scald_description)): ?>
+        <div class="atom__description">
+          <?php print $atom_wrapper->scald_description->value->value() ?>
         </div>
       <?php endif; ?>
     </div>
   </div>
 </div>
+
